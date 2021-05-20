3d7985eb51a103940e7731947cd0397497db7763
NeoPrint777 <drew@ronsman.com>
2021-05-20 06:46:01 -0500

    test

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as sg
 import random as rad
+import re
+import numpy as np
 def high_scores():
     score=rad.randint(1,10)
     a=sg.PopupGetText('Name')+': '+str(score)
@@ -8,12 +10,28 @@ def high_scores():
     f.close()
     print(a)
 def score_log():
+    layout = [[sg.Text('',key='-0-')], [sg.Text('',key='-1-')], [sg.Text('',key='-2-')]]
+    window=sg.Window('hi',layout)
+    score_value=[]
     f = open('scores/high_score', 'r')
     if f.mode=='r':
         contents=f.read()
         cool=list(contents.split('\n'))
-        for i in range(len(cool)):
-            name = cool[i]
-            print(name)
+        print(cool)
+        for i in range(len(cool)+1):
+            name,value= str(cool[i]).split(': ')
+            person = str(value+": "+name)
+            score_value.append(person)
+        score_value.sort()
+        score_value.reverse()
+        a=score_value
+        if len(score_value)>=3:
+            print(a[0:3])
+        print(a)
+        while True:
+            window.read(20)
+            window['-'+str(1)+'-'].update(a[1].split(':'))
+
     f.close()
+high_scores()
 score_log()

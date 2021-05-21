80a15ad9c8c0cfc23a47212d294b09145b30512e
NeoPrint777 <drew@ronsman.com>
2021-05-21 10:20:03 -0500

    test

--- snake_game.py
+++ snake_game.py
@@ -1,10 +1,48 @@
 import random as r
 import time
-
+from collections import namedtuple
 import PySimpleGUI as sg
 import keyboard as kb
-import time as t
+def leader_board_scores():
+    things = []
+    f = open('scores/high_score', 'r')
+    contents = f.read()
+    if len(contents) > 0:
+        cool = list(contents.split('\n'))
+        f.close()
+        cool.pop(len(cool) - 1)
+        scores = namedtuple('scores', 'name score')
+        for i in range(len(cool)):
+            name, value = str(cool[i - 1]).split(':')
+            person = scores(name, int(value))
+            things.append(person)
+        final = sorted(things, key=lambda x: getattr(x, 'score'), reverse=True)
+        if len(things)>=3:
+            for i in range(3):
+                new = final[i]
+                print(new)
+                name3, value3 = new.name, new.score
+                print(name3)
+                print(value3)
+                window[str(i) + '-'].update(str(name3) + ': ' + str(value3))
+        else:
+            for i in range(len(final)):
+                new = final[i]
+                print(new)
+                name3, value3 = new.name, new.score
+                print(name3)
+                print(value3)
+                window[str(i)+'-'].update(str(name3) + ': ' + str(value3))
+
+def high_scores(score):
+    a=sg.PopupGetText('Name')+':'+str(score)
+    f=open('scores/high_score', 'a')
+    f.write(a+'\n')
+    f.close()
+
+
 sg.theme('DarkPurple6')
+score=0
 grid_size=20
 snake= []
 head = []
@@ -14,34 +52,27 @@ x,y=5,5
 food =(10,10)
 cx,cy = 0, 0
 segments=3
-score_temp = sg.T('score:'+'\n'+str(score),key='-score-',size=(0,50),justification='top_center', font=('arial', 20),
+score_temp = sg.T(str(score),key='-score-',size=(30,2),justification='center', font=('arial', 20),
                   text_color='white')
 body = sg.Text('Snake',font=('arial', 20),justification='center',size=(60, 0))
 
 grid = ([sg.Button(key=(row, column), size=(2, 1), button_color=('white','black')) for row in range(grid_size)]
         for column in range(grid_size))
-leaderboard=[sg.Text()]
-layout = [[body], [sg.Frame('game', grid), score_temp]]
-window=sg.Window('hi', layout, finalize=True, size=(800,800))
 
+leaderboard=[[sg.Text('Score',justification='center',size=(20,0))],
+             [score_temp],
+             [sg.Column([sg.Text(' ', key=str(i)+'-', font=('Bold', 15),justification='center',size=(10,2))] for i in range(3))]]
+
+layout = [[body], [sg.Frame('game', grid), sg.Frame('Stats',leaderboard,font=('Bolded',15),border_width=10,size=(300,750),element_justification='center', vertical_alignment='top-center',background_color='DarkBlue')]]
+
+window=sg.Window('hi', layout, finalize=True, size=(900,750))
+leader_board_scores()
 window[food].update(button_color=('green', 'green'))
-def high_scores():
-    a=sg.PopupGetText('Name')+': '+str(score)
-    f=open('scores/high_score', 'a+')
-    f.write('\n'+str(a))
-    f.close()
-def score_log():
-    f = open('scores/high_score', 'r')
-    if f.mode=='r':
-        contents=f.read()
-        for i in range(len(contents)):
-            print(contents[i])
-    f.close()
+
 while True:
         window.read(20)
         if x>=grid_size or x<0 or y<0 or y>=grid_size:
-            high_scores()
-            #sg.popup('game over',font=('Bold',20))
+            high_scores(score)
             break
         else:
             if kb.is_pressed('d') and cx!=-1:
@@ -77,12 +108,7 @@ while True:
                     score=score+1
                     window['-score-'].update('score:  '+str(score))
             if head in snake[1:-1]: #death
-                high_scores()
                 break
             time.sleep(0.05)
 window.close()
-
-
-
-
 
--- test.py
+++ test.py
@@ -13,29 +13,28 @@ def score_log():
     window=sg.Window('hi',layout)
     things=[]
     f = open('scores/high_score', 'r')
-    if f.mode=='r':
-        contents=f.read()
-        cool=list(contents.split('\n'))
+    contents = f.read()
+    if len(contents)>0:
+        cool = list(contents.split('\n'))
         f.close()
         cool.pop(len(cool)-1)
         scores = namedtuple('scores', 'name score')
         for i in range(len(cool)):
-            name, value=str(cool[i-1]).split(':')
-            person=scores(name, int(value))
-            things.append(person)
+                name, value=str(cool[i-1]).split(':')
+                person=scores(name, int(value))
+                things.append(person)
         final=sorted(things, key=lambda x: getattr(x, 'name'), reverse=True)
         tok=1
-        print(name)
-        print(final)
         while True:
             window.read(20)
             if tok==1:
                 for i in range(3):
-                    new=final[i]
+                    new=final[i-1]
                     print(new)
                     name3, value3=new.name,new.score
                     print(name3)
                     print(value3)
                     window[str(i)].update(str(name3)+': '+str(value3))
                 tok=0
+high_scores()
 score_log()
